2b86f904b2891f2e868a13ee090d72c10e47a66d
IIIved <100132155+IIIved@users.noreply.github.com>
2022-05-16 14:14:03 +0300

    Initial commit

---
+++ single-linked-list/single-linked-list.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// добавьте неоходимые include-директивы сюда
+
+template <typename Type>
+class SingleLinkedList {
+	// напишите код класса тут
+};
+
+// внешние функции разместите здесь
